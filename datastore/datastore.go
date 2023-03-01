c195d80590bc762b7a6448f3df989f26dc3ec3e1
Kugayama Nana <nonamea.774@gmail.com>
2023-03-02 02:48:58 +0900

    change names

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,18 +2,11 @@ package datastore
 
 import (
 	"encoding/json"
+	"math"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
-)
-
-const (
-	partKey = "id"
-	sortKey = "name"
-
-	counterKind     = "counter"
-	counterValueKey = "value"
 )
 
 var (
@@ -21,59 +14,95 @@ var (
 	ErrNotFound = dynamo.ErrNotFound
 )
 
-type counterContainer struct {
-	Kind  string `dynamo:"id"`
-	Key   string `dynamo:"name"`
-	Value int    `dynamo:"value"`
-}
+type Order bool
 
-type objectContainer struct {
-	Kind string `dynamo:"id"`
-	Key  string `dynamo:"name"`
-	Item string `dynamo:"item"`
-}
+const (
+	Asc  Order = true
+	Desc Order = false
+
+	Inf int = math.MaxInt
+)
 
 type Client interface {
-	Put(kind string, key string, object interface{}) error
-	Get(kind string, key string) (string, error)
+	Put(name string, id int, object interface{}) error
+	Get(name string, id int) (string, error)
+	Take(name string, base int, cnt int, order Order) ([]string, error)
 
 	Inc(key string) (int, error)
 	// Top returns count of the key. if key does not exist, return (0, ErrNotFound)
 	Top(key string) (int, error)
 }
 
+const (
+	partKey = "id"
+	sortKey = "num"
+
+	counterValueKey = "val"
+
+	objectType  = "object--"
+	counterType = "counter--"
+)
+
+type base struct {
+	Name string `dynamo:"id"`
+	Id   int    `dynamo:"num"`
+}
+
+type counterContainer struct {
+	Base  base
+	Value int `dynamo:"val"`
+}
+
+type objectContainer struct {
+	Base base
+	Item string `dynamo:"obj"`
+}
+
 type client struct {
 	table *dynamo.Table
 }
 
-func (c *client) Put(kind string, key string, object interface{}) error {
+func (c *client) Put(name string, id int, object interface{}) error {
 	b, err := json.Marshal(object)
 	if err != nil {
 		return err
 	}
-	return c.table.Put(objectContainer{Kind: kind, Key: key, Item: string(b)}).Run()
+	container := objectContainer{
+		Base: base{
+			Name: objectType + name,
+			Id:   id,
+		},
+		Item: string(b),
+	}
+	return c.table.Put(container).Run()
 }
 
-func (c *client) Get(kind string, key string) (string, error) {
+func (c *client) Get(name string, id int) (string, error) {
 	buf := objectContainer{}
-	err := c.table.Get(partKey, kind).Range(sortKey, dynamo.Equal, key).One(&buf)
+	err := c.table.Get(partKey, objectType+name).Range(sortKey, dynamo.Equal, id).One(&buf)
 	return buf.Item, err
+}
+
+func (c *client) Take(name string, base int, cnt int, order Order) ([]string, error) {
+	return []string{}, nil
 }
 
 func (c *client) Inc(key string) (int, error) {
 	// ensure exists
 	buf := counterContainer{}
-	err := c.table.Update(partKey, counterKind).Range(sortKey, key).SetIfNotExists(counterValueKey, 0).Value(&buf)
+	dynamoKey := counterType + key
+	err := c.table.Update(partKey, dynamoKey).Range(sortKey, 0).SetIfNotExists(counterValueKey, 0).Value(&buf)
 	if err != nil {
 		return -1, err
 	}
-	err = c.table.Update(partKey, counterKind).Range(sortKey, key).SetExpr("'"+counterValueKey+"' = '"+counterValueKey+"' + ?", 1).Value(&buf)
+	err = c.table.Update(partKey, dynamoKey).Range(sortKey, 0).SetExpr("'"+counterValueKey+"' = '"+counterValueKey+"' + ?", 1).Value(&buf)
 	return buf.Value, err
 }
 
 func (c *client) Top(key string) (int, error) {
 	buf := counterContainer{}
-	err := c.table.Get(partKey, counterKind).Range(sortKey, dynamo.Equal, key).One(&buf)
+	dynamoKey := counterType + key
+	err := c.table.Get(partKey, dynamoKey).Range(sortKey, dynamo.Equal, 0).One(&buf)
 	return buf.Value, err
 }
 
